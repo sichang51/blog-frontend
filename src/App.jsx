65a22099e20d0a81ad4edfc228fa3cf44f5e2ad1
sichang51 <99237899+sichang51@users.noreply.github.com>
2023-11-06 17:53:21 -0800

    added each components to a function

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,78 @@
-function App() {
+function Header() {
   return (
     <div>
       <header>
         <a href="#">Home</a> | <a href="#posts-index">All posts</a> | <a href="#posts-new">New post</a>
       </header>
+    </div>
+  );
+}
 
-      <div id="posts-new">
-        <h1>New post</h1>
-        <h2>Enter your new blog posts here:</h2>
+function PostsNew() {
+  return (
+    <div id="posts-new">
+      <h1>New post</h1>
+      <h2>Enter your new blog posts here:</h2>
 
-        <form>
-          <label>
-            Name:
-            <input type="text" name="name" />
-          </label>
-          <br></br>
-          <br></br>
-          <input type="submit" value="submit" />
-        </form>
-
+      <form>
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
         <br></br>
         <br></br>
-        <h2>Enter your text here:</h2>
-        <textarea>Hello there!</textarea>
-      </div>
+        <input type="submit" value="submit" />
+      </form>
 
-      <div id="posts-index">
-        <h1>All posts</h1>
-        <h2>Week 3 Javascript Intro</h2>
-        <body>Introduction to JS. Worked on LOTR game and other exercises to learn and reinforce new language.</body>
+      <br></br>
+      <br></br>
+      <h2>Enter your text here:</h2>
+      <textarea>Hello there!</textarea>
+    </div>
+  );
+}
 
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Javascript_badge.svg/946px-Javascript_badge.svg.png"
-          width="200"
-          height="250"
-        />
-      </div>
+function PostsIndex() {
+  return (
+    <div id="posts-index">
+      <h1>All posts</h1>
+      <h2>Week 3 Javascript Intro</h2>
+      <body>Introduction to JS. Worked on LOTR game and other exercises to learn and reinforce new language.</body>
 
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Javascript_badge.svg/946px-Javascript_badge.svg.png"
+        width="200"
+        height="250"
+      />
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <div>
       <footer>
         <p>Copyright 20XX</p>
       </footer>
+    </div>
+  );
+}
+
+function Content() {
+  return (
+    <div>
+      <PostsNew />
+      <PostsIndex />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
