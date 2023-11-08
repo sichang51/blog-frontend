54d26febeea0330a28cc42e6655796258b949874
sichang51 <99237899+sichang51@users.noreply.github.com>
2023-11-07 17:47:45 -0800

    added useState hook in Content.jsx, setPosts in handleIndexPosts

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,34 +1,15 @@
 import { PostsIndex } from "./PostsIndex";
 import { PostsNew } from "./PostsNew";
 import axios from "axios";
+import { useState } from "react";
 
 export function Content() {
-  let posts = [
-    // {
-    //   id: 1,
-    //   title: "title1",
-    //   body: "Introduction to JS. Worked on LOTR game and other exercises to learn and reinforce new language.",
-    //   image:
-    //     "https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Javascript_badge.svg/946px-Javascript_badge.svg.png",
-    // },
-    // {
-    //   id: 2,
-    //   title: "title2",
-    //   body: "body2",
-    //   image: "___",
-    // },
-    // {
-    //   id: 3,
-    //   title: "title3",
-    //   body: "body3",
-    //   image: "___",
-    // },
-  ];
+  const [posts, setPosts] = useState([]);
 
   const handleIndexPosts = () => {
     axios.get("http://localhost:3000/posts.json").then((response) => {
       console.log(response.data);
-      posts = response.data;
+      setPosts(response.data);
     });
   };
 
