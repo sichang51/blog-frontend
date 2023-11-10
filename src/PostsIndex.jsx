0e304ee9dac701ac1fbee9ef048c279f20fcb005
sichang51 <99237899+sichang51@users.noreply.github.com>
2023-11-09 19:53:57 -0800

    added multi-columns to PostsIndex.jsx

--- src/PostsIndex.jsx
+++ src/PostsIndex.jsx
@@ -1,20 +1,23 @@
 export function PostsIndex(props) {
   console.log(props.myPosts);
   return (
-    <div id="posts-index">
+    <div id="posts-index" className="container text-center">
       <div className="header-all-posts">
         <h1>All posts</h1>
       </div>
-
-      {props.myPosts.map((post) => (
-        <div key={post.id} className="blogs">
-          <h3>{post.title}</h3>
-          <img src={post.image} alt="" height="200px" width="50px" />
-          {/* <p>{post.body}</p> */}
-          <br></br>
-          <button onClick={() => props.onShowPost(post)}>More Info</button>
-        </div>
-      ))}
+      <div className="row">
+        {props.myPosts.map((post) => (
+          <div key={post.id} className="blogs col-lg-4 col-md-6 col-12 my-3">
+            <h3>{post.title}</h3>
+            <img src={post.image} alt="" height="200px" width="50px" />
+            {/* <p>{post.body}</p> */}
+            <br></br>
+            <button className="btn btn-secondary" onClick={() => props.onShowPost(post)}>
+              More Info
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
