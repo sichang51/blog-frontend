000c5479f51352448080e07c7a38e14d38b269c7
sichang51 <99237899+sichang51@users.noreply.github.com>
2023-11-09 18:54:12 -0800

    added new classes and params in PostsNew.jsx

--- src/PostsNew.jsx
+++ src/PostsNew.jsx
@@ -5,14 +5,14 @@ export function PostsNew() {
         <h1>New Posts</h1>
       </div>
       <h2>Enter your new blog posts here:</h2>
-      <form>
+      {/* <form>
         <label>
           <input type="text" placeholder="Title" />
-        </label>
-        {/* <br></br>
+        </label> */}
+      {/* <br></br>
         <br></br>
         <input type="submit" value="submit" /> */}
-      </form>
+      {/* </form>
       <form>
         <label>
           <input className="blog-textbox" type="text" placeholder="Enter your blog text here." />
@@ -25,7 +25,17 @@ export function PostsNew() {
       <h2>Upload image here:</h2>
       <div className="imagebox">Enter your image URL here.</div>
       <br></br>
-      <button className="post-button">Push</button>
+      <button className="post-button">Push</button> */}
+
+      <div className="form-group">
+        New Post Title: <input className="form-control" type="text" name="title" />
+      </div>
+      <div className="form-group">
+        New Blog Post: <input className="form-control" type="text" name="body" />
+      </div>
+      <div className="form-group">
+        New Image URL: <input className="form-control" type="text" name="image" />
+      </div>
     </div>
   );
 }
