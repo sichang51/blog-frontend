449b60000426673348642d039274d1f49875ffd5
sichang51 <99237899+sichang51@users.noreply.github.com>
2023-11-19 08:47:17 -0800

    fixed error on PostsShowPage

--- src/PostsShowPage.jsx
+++ src/PostsShowPage.jsx
@@ -5,23 +5,23 @@ import { useParams } from "react-router-dom";
 export function PostsShowPage() {
   const [post, setPost] = useState({});
   const params = useParams();
+
+  const handleShowPost = () => {
+    console.log({ "Post id is": params.id });
+    axios.get(`http://localhost:3000/posts/${params.id}.json`).then((response) => {
+      console.log(response);
+      setPost(response.data);
+    });
+  };
+
+  useEffect(handleShowPost, []);
+
+  return (
+    <div>
+      <h1>Post Info</h1>
+      <img src={post.image} />
+      <h4>{post.title}</h4>
+      <p>{post.body}</p>
+    </div>
+  );
 }
-
-const handleShowPost = () => {
-  console.log({ "Post id is": params.id });
-  axios.get(`http://localhost:3000/posts/${params.id}.json`).then((response) => {
-    console.log(response);
-    setPost(response.data);
-  });
-};
-
-useEffect(handleShowPost, []);
-
-return (
-  <div>
-    <h1>Post Info</h1>
-    <img src={post.image_url} />
-    <h4>{post.title}</h4>
-    <p>{post.body}</p>
-  </div>
-);
