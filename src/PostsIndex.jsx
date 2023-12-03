de7a8756c52d1a3fca770cbe20f257dc1163a9a3
sichang51 <99237899+sichang51@users.noreply.github.com>
2023-12-03 08:35:59 -0800

    added search function in PostsIndex.jsx which includes lowercase character to filter on post names

--- src/PostsIndex.jsx
+++ src/PostsIndex.jsx
@@ -1,23 +1,31 @@
+import { useState } from "react";
+
 export function PostsIndex(props) {
   console.log(props.myPosts);
+  const [searchFilter, setSearchFilter] = useState("");
+
   return (
     <div id="posts-index" className="container text-center">
       <div className="header-all-posts">
         <h1>All posts</h1>
+        Search Filter:{" "}
+        <input type="text" value={searchFilter} onChange={(event) => setSearchFilter(event.target.value)} />
       </div>
       <div className="row">
-        {props.myPosts.map((post) => (
-          <div key={post.id} className="blogs col-lg-4 col-md-6 col-12 my-3">
-            <h3>{post.title}</h3>
-            <img src={post.image} alt="" height="200px" width="50px" />
-            {/* <p>{post.body}</p> */}
-            <br></br>
-            <br></br>
-            <button className="btn btn-primary" onClick={() => props.onShowPost(post)}>
-              More Info
-            </button>
-          </div>
-        ))}
+        {props.myPosts
+          .filter((post) => post.title.toLowerCase().includes(searchFilter.toLowerCase()))
+          .map((post) => (
+            <div key={post.id} className="blogs col-lg-4 col-md-6 col-12 my-3">
+              <h3>{post.title}</h3>
+              <img src={post.image} alt="" height="200px" width="50px" />
+              {/* <p>{post.body}</p> */}
+              <br></br>
+              <br></br>
+              <button className="btn btn-primary" onClick={() => props.onShowPost(post)}>
+                More Info
+              </button>
+            </div>
+          ))}
       </div>
     </div>
   );
