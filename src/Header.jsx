0a56aac67ea6b90e16817be97abec9fe9d0c21be
sichang51 <99237899+sichang51@users.noreply.github.com>
2023-12-03 07:58:51 -0800

    added LogoutLink to be functional as href

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,30 +1,9 @@
+import { LogoutLink } from "./LogoutLink";
 import { Link } from "react-router-dom";
 
 export function Header() {
   return (
     <div>
-      {/* <div className="header-block"> */}
-      {/* <br></br>
-        <br></br> */}
-      {/* <a href="#home">
-          <button className="header-home-button">Home</button>
-        </a>{" "}
-        |{" "}
-        <a href="#posts-index">
-          <button className="header-all-posts-button">All posts</button>
-        </a>{" "}
-        |{" "}
-        <a href="#posts-new">
-          <button className="header-new-post-button">New post</button>
-        </a>
-      </div>
-      <img
-        src="https://lh5.googleusercontent.com/QFTu6llopUzF38mRPbobI3C0OQG6D0qprr5dQkv4-pe-rXlh_GKlwvQtZaWUytAp6EY4mnNvytRYp8bvbiYeMFefF9Fldv129KwlNY5BnOCMvBYm0p64nLlfNV68lof6LGdfhoL8"
-        width={180}
-        height={90}
-        style={{ alignSelf: "center" }}
-        alt="Blog Logo"
-      /> */}
       <nav className="navbar navbar-expand-lg" style={{ backgroundColor: "#0d6efd" }} data-bs-theme="dark">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
@@ -58,21 +37,24 @@ export function Header() {
                   New Posts
                 </Link>
               </li>
-              <li className="nav-item">
-                <Link to="/signup" className="nav-link" style={{ textDecoration: "none" }}>
-                  Signup
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/login" className="nav-link" style={{ textDecoration: "none" }}>
-                  Login
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/logoutLink" className="nav-link" style={{ textDecoration: "none" }}>
-                  Logout
-                </Link>
-              </li>
+              {localStorage.jwt === undefined ? (
+                <>
+                  <li className="nav-item">
+                    <Link to="/signup" className="nav-link" style={{ textDecoration: "none" }}>
+                      Signup
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link to="/login" className="nav-link" style={{ textDecoration: "none" }}>
+                      Login
+                    </Link>
+                  </li>
+                </>
+              ) : (
+                <li className="nav-item">
+                  <LogoutLink />
+                </li>
+              )}
             </ul>
           </div>
         </div>
